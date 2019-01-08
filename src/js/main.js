ee7c97c47c23b43af0160ed452b13b523fb20cdc
Ssonic8 <gladunalexv@gmail.com>
2019-01-08 17:00:37 +0300

    section footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	let promoClose = document.querySelector(".header__promo-button"),
+			promoElement = document.querySelector(".header__promo");
+	promoClose.addEventListener("click", function(evt){
+		evt.preventDefault();
+		promoElement.classList.add('visually-hidden');
+	});	
+
 	$('.owl-carousel').owlCarousel({
 		items: 4,
 		loop: true,
